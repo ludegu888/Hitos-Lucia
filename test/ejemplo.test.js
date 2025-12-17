1ac5676c4e288886f4ea4ad5f9c9dfea1f532bad
Lucia Delgado <ludegu888@gmail.com>
2025-12-17 12:52:54 +0100

    Fix test expectation for suma simple

--- test/ejemplo.test.js
+++ test/ejemplo.test.js
@@ -2,5 +2,5 @@ const ejemplo = require('../ejemplo');
 
 test('suma simple', () => {
   // Esto fallará a propósito
-  expect(ejemplo.sumar(1, 1)).toBe(3);
+  expect(ejemplo.sumar(1, 1)).toBe(2);
 });
